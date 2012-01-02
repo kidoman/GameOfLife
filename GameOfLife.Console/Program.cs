f24c29dd4432e881ede304f1ddd6c335714ab381
Karan Misra <karan@erodov.com>
2012-01-02 20:51:49 +0530

    Created initial project structure

---
+++ GameOfLife.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GameOfLife.Console {
+    using System;
+
+    public class Program {
+        static void Main(string[] args) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameOfLife.Tests/Tests/GameOfLifeSolverTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class GameOfLifeSolverTests {
+}
+
+public class GameOfLifeSolver : IGameOfLifeSolver {
+    public bool[,] Solve(bool[,] input, int moves) {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IGameOfLifeSolver {
+    bool[,] Solve(bool[,] input, int moves);
+}
